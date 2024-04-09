2a1985e21923e1e94dbce23f2fa75953eb6c7d13
Christian Reiniger <creinig@creinig.de>
2024-04-09 09:41:28 +0200

    initial setup

---
+++ lib/mvnrepocopy.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "mvnrepocopy/version"
+
+module Mvnrepocopy
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_mvnrepocopy.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TestMvnrepocopy < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Mvnrepocopy::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
